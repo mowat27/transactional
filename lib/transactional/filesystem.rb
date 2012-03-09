59a98818d91297cd548d295ef7bf52bdde3449d7
Adrian Mowat <adrian.mowat@gmail.com>
2012-03-09 14:00:31 +0000

    Rolled Filesystem up into TDir

--- lib/transactional/filesystem.rb
+++ lib/transactional/filesystem.rb
@@ -1,32 +1,14 @@
 module Transactional
   class FileSystem < TDir
     def initialize(transaction, root)
-      @path = root
-      @tfiles = []
-      @tdirs = []
+      super(root, "")
       @transaction = transaction
     end
 
     def open(rpath)
-      @tfiles << TFile.load(@path, rpath)
-      @tfiles.last.open {|f| yield f if block_given?}
+      super
       rescue Exception => ex
         @transaction.rollback
-    end
-
-    def create_directory(rpath)
-     tdir = TDir.new(@path, rpath)
-     tdir.create
-     @tdirs << tdir
-    end
-
-    def commit
-      @tfiles.each {|tfile| tfile.commit}
-    end
-
-    def rollback
-      @tdirs.each  {|tdir|  tdir.rollback}
-      @tfiles.each {|tfile| tfile.rollback}
     end
   end
 end
